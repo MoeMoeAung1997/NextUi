33f3fb9c25bfa7468d5cfc505f00560e478a99f5
MohMohAung <mohmohaung737@gmail.com>
2023-08-30 01:26:25 +0630

    theme

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,61 +1,13 @@
-"use client";
 import { Card } from "@nextui-org/react";
-//import {AcmeLogo} from "./AcmeLogo.jsx";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-} from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
-import Link from "next/link";
-export default function Home() {
-  return (
-    <Navbar style={{ backgroundColor: "black" }}>
-      <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-      <NavbarContent className=" sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link href="#" color="gray">
-            Docs
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Components
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="red" href="#">
-            Blog
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="red" href="#">
-            Figma
-          </Link>
-        </NavbarItem>
 
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            New components v2.1.0
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-  );
+
+export default function Home(){
+    return(
+
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
 }
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,8 +1,17 @@
 // app/providers.tsx
 "use client";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
-
+import NavbarPage from "./nav/Navbar";
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <React.StrictMode>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <NavbarPage>{children}</NavbarPage>
+        </main>
+      </NextUIProvider>
+    </React.StrictMode>
+  );
 }
