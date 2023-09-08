fd08ce338213d3aaf85bb223bc8cccfd55bc7d80
MohMohAung <mohmohaung737@gmail.com>
2023-09-08 14:34:43 +0630

    add: navbar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,16 @@
+"use client";
 import { Card } from "@nextui-org/react";
 
-
-export default function Home(){
-    return(
-
-        <div>
-            <div>
-                
-            </div>
-        </div>
-    )
+export default function Home() {
+  return (
+    <Card>
+      <div>
+        <h1>
+          Make <span>beautiful</span>websites regardless of your design
+          experience.
+        </h1>
+        <h3>Beautiful, fast and modern React UI library.</h3>
+      </div>
+    </Card>
+  );
 }
--- src/app/nav/Navbar.tsx
+++ src/app/nav/Navbar.tsx
@@ -10,13 +10,104 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
+import cilChevronBottom from "@coreui/icons";
+import CIcon from "@coreui/icons-react";
+import * as icon from "@coreui/icons";
+import {
+  Button,
+  ButtonGroup,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/react";
 import Link from "next/link";
+import { useState } from "react";
+import React from "react";
+import { ReactNode } from "react";
+
+export type DropdownItemSelectedProps = {
+  icon?: ReactNode;
+  isSelected?: boolean;
+  isDisabled?: boolean;
+};
+
+//type selectedIcon?: ReactNode | (( props: DropdownItemSelectedProps) => ReactNOde) | null;
 export default function NavbarPage() {
+  const [selectedOption, setSelectedOption] = React.useState(
+    new Set(["merge"])
+  );
+
+  //type selectedIcon?: ReactNode | (( props: DropdownItemSelectedProps) => ReactNOde) | null;
+
+  interface descriptionMapProps {
+    merge: string;
+    squash: string;
+    //rebase: string;
+  }
+  const descriptionMap: descriptionMapProps[] = [
+    {
+      merge: "v1.1.1",
+      squash: "v2.1.1",
+      //squash: "All People",
+      //rebase: "All animals",
+    },
+    // {
+    //   merge: "All content",
+    //   squash: "All People",
+    //   //rebase: "All animals",
+    // },
+  ];
+
+  const labelMap: { [key: string]: string } = {
+    merge: "v2.1.0",
+    squash: "v1.1.1",
+    //squash: "squash and merge",
+    //rebase: "Rebase and merge",
+  };
+
+  const selectedOptionValue: string = Array.from(selectedOption)[0];
   return (
     <Navbar style={{ backgroundColor: "black" }}>
       <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
+        <h1 className="font-bold  text-xxl">Next UI</h1>
+        {/* <Button className="heigh-10">v2.1.10</Button> */}
+
+        <ButtonGroup variant="flat">
+          <Button size="sm">{labelMap[selectedOptionValue]}</Button>
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Button isIconOnly size="sm">
+                <CIcon icon={icon.cilChevronBottom} size="sm" height={20} />
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              disallowEmptySelection
+              aria-label="Merge options"
+              selectedKeys={selectedOption}
+              selectionMode="single"
+              onSelectionChange={setSelectedOption}
+              className="max-w-[300px]"
+            >
+              <DropdownItem key="merge" description={descriptionMap[0].merge}>
+                {labelMap["merge"]}
+              </DropdownItem>
+              <DropdownItem key="squash" description={descriptionMap[0].merge}>
+                {labelMap["squash"]}
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </ButtonGroup>
+        {/* <Dropdown>
+          <DropdownTrigger>
+            <Button>v2.1.10</Button>
+            <CIcon icon={icon.cilChevronBottom} />
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownItem key="new">v2.1.10</DropdownItem>
+            <DropdownItem key="new">v1.0.0</DropdownItem>
+          </DropdownMenu>
+        </Dropdown> */}
       </NavbarBrand>
       <NavbarContent className=" sm:flex gap-4" justify="center">
         <NavbarItem>
